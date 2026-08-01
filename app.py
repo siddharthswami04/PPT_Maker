6874b5b1be212260bbef919e86fc8996b5f0c214
Siddharth <siddharthswami40@gmail.com>
2026-08-01 11:04:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -124,17 +124,17 @@ if (user_input):
         with st.spinner("Running Agent"):
             try:
 
-                prompt = """Give Latest News Related to Given user Query
-                in Dynamic HTML, Output with cards Design Format.
-                Strict HTML Output, No Any markdowns Repsonse
+              prompt = """Give Latest News Related to Given user Query
+              in Dynamic HTML, Output with cards Design Format.
+              Strict HTML Output, No Any markdowns Repsonse
 
-                User Query: """ + user_input
+              User Query: """ + user_input
 
-                response = leader_agent.invoke({
-                    'messages': [{'role': 'user','content': prompt}]})
+              response = leader_agent.invoke({
+                  'messages': [{'role': 'user','content': prompt}]})
 
-                code = response['messages'][-1].content[-1]['text']
-                st.html(code, width="stretch", unsafe_allow_javascript=True)
+              code = response['messages'][-1].content[-1]['text']
+              st.html(code, width="stretch", unsafe_allow_javascript=True)
 
             except Exception as err:
                 st.error("Error Code: ", err)
