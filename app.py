2bd8da744384a3489b53aa3d3acd0898a45dcdec
Siddharth <siddharthswami40@gmail.com>
2026-08-01 10:59:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,10 +89,10 @@ def run_agent(leader_agent,query):
 if all(ALL_API):
   leader_agent = create_agent(
     model=model,
-    tools=[search_latest_info
-          #  generate_image,
+    tools=[search_latest_info,
+           #  generate_image
            ])
-  leader_agent
+  # leader_agent
 
 else:
   st.info("Give API Keys first to load agent")
